9acbce09629da4ff4b9f9a2d88f9138e624c3847
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2021-10-23 12:58:17 -0300

    Actualizamos lo referente a la primer practico, y algunas modificaciones en clase 1

---
+++ clase 1/clase1.js
@@ -0,0 +1,27 @@
+// Comentario una linea
+/* Comentario multilinea */
+// Variables
+
+// Declaracion en ES5
+var nombreVariable;
+
+// Declaracion en ES6
+let nombreVariable2;
+let edadUsuario;
+
+// Constantes
+const LENGUAJE = "JAVASCRIPT"
+
+nombreVariable = "Jose";
+edadUsuario = 20;
+
+console.log(nombreVariable);
+console.log(edadUsuario);
+
+
+//suma
+let suma = 30 + 40
+
+console.log('La suma es igual: ' + suma);
+
+
---
+++ practico 1/practico1.js
@@ -0,0 +1,9 @@
+// Avisamos al usr por panntalla de que se trata esta funcion
+alert('Funcion suma con js')
+
+// Pedimos carga por pantalla de los dos valores, y loss pasamos a numeros flotantes
+let valor1 = parseFloat(prompt('Ingrese un número: '))
+let valor2= parseFloat(prompt('Ingrese otro número: '))
+
+// Mostramos por pantalla el resultado
+alert('La suma entre ' + valor1 + ' y ' + valor2 + ' es igual a: ' + (valor1 + valor2))
