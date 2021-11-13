ec6093d5924b170bf047d78e0690c11cf8e1188f
Gustavo Ferreri <ferreri.gustavo@gmail.com>
2021-11-13 09:20:14 -0300

    Agregamos funciones DOM, y arrays, para los objetivos de trabajo practico

--- practico 6/practico6.js
+++ practico 6/practico6.js
@@ -7,6 +7,7 @@
 
 // Establecemos el periodo anual en dias
 const anno = 360;
+const diaHoy = new Date(Date.now());
 
 // Debemos Aplicar objetos en JS
 // Objeto plazoFijo
@@ -34,10 +35,25 @@ class plazoFijo{
         return this.montoInvertido+this.interesMensualPercibido();
     }
 }
+// Fomateamos fecha
+function formatoFecha(fecha, formato) {
+    const map = {
+        dd: fecha.getDate(),
+        mm: fecha.getMonth() + 1,
+        yy: fecha.getFullYear().toString().slice(-2),
+        yyyy: fecha.getFullYear()
+    }
+
+    return formato.replace(/dd|mm|yy|yyy/gi, matched => map[matched])
+}
+// Creamos una funcion para obtener la fecha destino, por el momento de corrido
+function plazoCierre(fecha, dias){
+    fecha.setDate(fecha.getDate() + dias);
+    return fecha;
+}
 
 // Mostramos los resultados por pantalla
 function showResult() {   
-
     // creamos el objeto con los datos del plazo fijo, tomando los datos del formulario
     let usrPf = new plazoFijo(
         parseInt(document.getElementsByName("monto")[0].value), 
@@ -45,21 +61,34 @@ function showResult() {
         parseInt(document.getElementsByName("plazo")[0].value)
         );
 
-    // Insertamos los resultados en el HTML, en este caso implementando Objetos
-    document.getElementById("resultado").innerHTML = `
-        <p>Monto invertido: $ ${usrPf.montoInvertido.toFixed(2)}</p>
-        <p>Interes anual nominal(TNA): ${usrPf.interesAnual.toFixed(2)}%</p>
-        <p>Plazo: ${usrPf.plazo} dias</p>
-        <p>Tasa mensual: ${usrPf.interesMensual().toFixed(2)}%</p>
-        <p>Tasa efectiva anual (TEA): ${usrPf.tasaEfectivaAnual()}%</p>
-        <p>Interes mensual percibido: $${usrPf.interesMensualPercibido().toFixed(2)}</p>
-        <p>Total percibido en el periodo: $${usrPf.totalPercibido().toFixed(2)}</p>`;
+    // Mostramos los resultados por pantalla pero en esta ocasion usamos una tabla
+    let table = document.getElementById("resultado");
+    let row = table.insertRow(1);
+    let cell= [];
+    for (j=0; j<8; j++) {cell[j] = row.insertCell(j);}
+    cell[1].innerHTML = formatoFecha(diaHoy, "dd/mm/yy");
+    cell[2].innerHTML = usrPf.montoInvertido;
+    cell[3].innerHTML = usrPf.plazo;
+    cell[4].innerHTML = usrPf.interesAnual.toFixed(2) + "%";
+    cell[5].innerHTML = usrPf.tasaEfectivaAnual() + "%";
+    cell[6].innerHTML = formatoFecha(plazoCierre(diaHoy, usrPf.plazo), "dd/mm/yy");
+    cell[7].innerHTML = "$" + usrPf.totalPercibido().toFixed(2);
 
-    // Mostramos los resultados en la consola
-    console.log("Se invirtio: $" + usrPf.montoInvertido);
-    console.log("En un plazo de " + usrPf.plazo + " dias");
-    console.log("La tasa anual nominal (TNA) es: " + usrPf.interesAnual.toFixed(2)  + "%");
-    console.log("La tasa anual efectiva (TEA) es: " + usrPf.tasaEfectivaAnual() + "%");
-    console.log("El interes mensual percibido es: $" + usrPf.interesMensualPercibido().toFixed(2));
-    console.log("El total percibido en el periodo es: $" + usrPf.totalPercibido().toFixed(2));
+    /* Comentamos la funciones de mostrar utilizadas en los practicos anteriores */
+    // Insertamos los resultados en el HTML, en este caso implementando Objetos
+    // document.getElementById("resultado").innerHTML = `
+    //     <p>Monto invertido: $ ${usrPf.montoInvertido.toFixed(2)}</p>
+    //     <p>Interes anual nominal(TNA): ${usrPf.interesAnual.toFixed(2)}%</p>
+    //     <p>Plazo: ${usrPf.plazo} dias</p>
+    //     <p>Tasa mensual: ${usrPf.interesMensual().toFixed(2)}%</p>
+    //     <p>Tasa efectiva anual (TEA): ${usrPf.tasaEfectivaAnual()}%</p>
+    //     <p>Interes mensual percibido: $${usrPf.interesMensualPercibido().toFixed(2)}</p>
+    //     <p>Total percibido en el periodo: $${usrPf.totalPercibido().toFixed(2)}</p>`;
+    // // Mostramos los resultados en la consola
+    // console.log("Se invirtio: $" + usrPf.montoInvertido);
+    // console.log("En un plazo de " + usrPf.plazo + " dias");
+    // console.log("La tasa anual nominal (TNA) es: " + usrPf.interesAnual.toFixed(2)  + "%");
+    // console.log("La tasa anual efectiva (TEA) es: " + usrPf.tasaEfectivaAnual() + "%");
+    // console.log("El interes mensual percibido es: $" + usrPf.interesMensualPercibido().toFixed(2));
+    // console.log("El total percibido en el periodo es: $" + usrPf.totalPercibido().toFixed(2));
 }
